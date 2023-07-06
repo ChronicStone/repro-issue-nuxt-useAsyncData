c76b0f03d61374c8ecd01851295ec28666b3a0e7
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2023-07-06 19:46:08 +0200

    chore: simplify repro

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+const id = ref<number>(0)
+
+const increment = () => id.value++
+const decrement = () => id.value--
+</script>
+
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+    <button @click="increment">+</button>
+    id: {{  id  }}
+    <button @click="decrement">-</button>
+    <Profile :id="id" />
 </template>
